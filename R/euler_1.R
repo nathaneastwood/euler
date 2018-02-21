8a0bacea2be7b79918b78f40b67227cd1f217108
Nicola Crane <ncrane@Mango.local>
2018-02-21 10:55:13 +0000

    perf: Vectorised euler_1

--- R/euler_1.R
+++ R/euler_1.R
@@ -11,19 +11,7 @@
 #' @export
 euler_1 <- function(x) {
 
-  threes = NULL
-  fives = NULL
-  fifteens = NULL
-  for(i in 1:(x-1)){
-    if(i%%3==0){
-      threes = c(threes, i)
-    }
-    if(i%%5==0){
-      fives = c(fives, i)
-    }
-    if(i%%15==0){
-      fifteens = c(fifteens, i)
-    }
-  }
-  sum(threes) + sum(fives) - sum(fifteens)
+  vals = 1:(x-1)
+  sum(vals[vals%%3==0|vals%%5==0])
+
 }
