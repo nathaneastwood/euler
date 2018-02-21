69887d832f8528d7f23adbdc1e4a3d00dda4a823
Nicola Crane <ncrane@Mango.local>
2018-02-21 10:16:04 +0000

    Add euler_1 functions

---
+++ R/euler_1.R
@@ -0,0 +1,15 @@
+#' Project Euler 1: Multiples of 3 and 5
+#'
+#' This is a solution to
+#' \href{https://projecteuler.net/problem=1}{Euler problem 1}.
+#'
+#' @param x Integer to find multiples under
+#' @param y Integer vector of values to find multiple of
+#'
+#' @examples
+#' euler_1()
+#'
+#' @export
+euler_1 <- function(x) {
+
+}
