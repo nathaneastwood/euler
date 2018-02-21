96213951347385c0bcdf48c507f94fa7c9bc9404
Nicola Crane <ncrane@Mango.local>
2018-02-21 10:20:41 +0000

    Update euler_1

--- R/euler_1.R
+++ R/euler_1.R
@@ -3,13 +3,27 @@
 #' This is a solution to
 #' \href{https://projecteuler.net/problem=1}{Euler problem 1}.
 #'
-#' @param x Integer to find multiples under
-#' @param y Integer vector of values to find multiple of
+#' @param x Max value
 #'
 #' @examples
-#' euler_1()
+#' euler_1(1000)
 #'
 #' @export
 euler_1 <- function(x) {
 
+  threes = NULL
+  fives = NULL
+  fifteens = NULL
+  for(i in 1:(x-1)){
+    if(i%%3==0){
+      threes = c(threes, i)
+    }
+    if(i%%5==0){
+      fives = c(fives, i)
+    }
+    if(i%%15==0){
+      fifteens = c(fifteens, i)
+    }
+  }
+  sum(threes) + sum(fives) - sum(fifteens)
 }
