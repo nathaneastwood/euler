42ec9549efa3368f3caafd8b6555103e6760c01b
Nicola Crane <ncrane@Mango.local>
2018-02-21 11:10:34 +0000

    feat: Add a solution to Euler 2

---
+++ R/euler_2.R
@@ -0,0 +1,30 @@
+#' Project Euler 2: Even Fibonacci numbers
+#'
+#' This is a solution to
+#' \href{https://projecteuler.net/problem=2}{Euler problem 2}.
+#'
+#' @param x Max value
+#'
+#' @examples
+#' euler_2(4000000)
+#'
+#' @export
+euler_2 <- function(x) {
+
+  vals = c(1, 2)
+
+  for(i in 3:x){
+    newval = vals[i-1] + vals[i-2]
+    if(newval > x){
+      break()
+    }
+    else{
+      vals = c(vals, newval)
+    }
+  }
+
+  sum(vals[vals%%2==0])
+
+}
+
+
--- tests/testthat/test_euler.R
+++ tests/testthat/test_euler.R
@@ -4,6 +4,10 @@ test_that("Test Euler problem 1", {
   expect_equal(euler_1(1000), 233168L)
 })
 
+test_that("Test Euler problem 2", {
+  expect_equal(euler_2(4000000), 4613732L)
+})
+
 test_that("Test Euler problem 11", {
   expect_equal(euler_11(data_11), 70600674L)
 })
