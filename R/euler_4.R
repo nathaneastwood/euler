2e6f774e0f298554d3e4b52c2b2eb0ae733eaba7
Nicola Crane <ncrane@Mango.local>
2018-02-22 09:36:50 +0000

    feat: Add a solution to Euler 4

--- R/euler_4.R
+++ R/euler_4.R
@@ -1,19 +1,42 @@
-#' Project Euler 4: Largest prime factor
+#' Project Euler 4: Largest palindrome product
 #'
 #' This is a solution to
-#' \href{https://projecteuler.net/problem=5}{Euler problem 5}.
+#' \href{https://projecteuler.net/problem=4}{Euler problem 4}.
+#'
+#' @param x N digits
 #'
 #' @examples
-#' euler_5()
+#' euler_4(600851475143)
 #'
 #' @export
-euler_5 <- function(x) {
+euler_4 <- function(x) {
 
-  #
-  #
-  # 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
-  #
-  # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+  max_val = (10^x)-1
+  min_val = (10^(x-1)) + 1
+  palindrome = NULL
 
+  for(i in max_val:min_val){
+
+    for(j in max_val:min_val){
+
+
+      num = as.character(i*j)
+      mid = round((nchar(num)/2)-0.1)
+      first_half = substr(num, 1, mid)
+
+      second_half = substr(num, (nchar(num)-mid)+1, nchar(num))
+      second_half_reversed = paste(rev(strsplit(second_half, "")[[1]]), collapse="")
+
+      if(first_half == second_half_reversed){
+        palindrome = c(num, palindrome)
+        break()
+
+      }
+
+
+    }
+  }
+
+  max(as.numeric(palindrome))
 
 }
