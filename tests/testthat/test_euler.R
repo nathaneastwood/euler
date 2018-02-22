a7cdd4b6c4819a8b1f2ce2c97db5418c3ea7e3ed
Nicola Crane <ncrane@Mango.local>
2018-02-22 09:37:40 +0000

    test: Add a test to Euler 4

--- tests/testthat/test_euler.R
+++ tests/testthat/test_euler.R
@@ -12,6 +12,10 @@ test_that("Test Euler problem 3", {
   expect_equal(euler_3(600851475143), 6857L)
 })
 
+test_that("Test Euler problem 4", {
+  expect_equal(euler_4(3), 906609L)
+})
+
 test_that("Test Euler problem 11", {
   expect_equal(euler_11(data_11), 70600674L)
 })
