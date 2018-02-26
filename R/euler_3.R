2e61a072b4dab3aa11b781d94f369ca7a859e333
nathaneastwood <neastwood@mango-solutions.com>
2018-02-26 16:10:17 +0000

    refactor: Correct code to conform with standards doc: Make documentation clearer

--- R/euler_3.R
+++ R/euler_3.R
@@ -3,7 +3,7 @@
 #' This is a solution to
 #' \href{https://projecteuler.net/problem=3}{Euler problem 3}.
 #'
-#' @param x Value
+#' @param x A number of interest to find the largest prime factor of.
 #'
 #' @examples
 #' euler_3(600851475143)
@@ -11,18 +11,18 @@
 #' @export
 euler_3 <- function(x) {
 
-  factors = find_factors(x)
+  factors <- find_factors(x)
 
-  prime_factors = NULL
+  prime_factors <- NULL
 
-  for(i in seq_along(factors)){
+  for (i in seq_along(factors)) {
 
-    if(is_prime(factors[[i]][1])){
-      prime_factors = c(prime_factors, factors[[i]][1])
+    if (is_prime(factors[[i]][1])) {
+      prime_factors <- c(prime_factors, factors[[i]][1])
     }
 
-    if(is_prime(factors[[i]][2])){
-      prime_factors = c(prime_factors, factors[[i]][2])
+    if (is_prime(factors[[i]][2])) {
+      prime_factors <- c(prime_factors, factors[[i]][2])
     }
   }
 
