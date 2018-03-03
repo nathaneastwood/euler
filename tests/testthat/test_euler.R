ea3ef2c3a87f3fcc4a7d3851fb0ac36d0a8e54dd
Nicola Crane <ncrane@Mango.local>
2018-03-03 18:09:59 +0000

    feat: add solution to Euler 13

--- tests/testthat/test_euler.R
+++ tests/testthat/test_euler.R
@@ -32,6 +32,10 @@ test_that("Test Euler problem 11", {
   expect_equal(euler_11(data_11), 70600674L)
 })
 
+test_that("Test Euler problem 13", {
+  expect_equal(euler_13(data_13), 5537376230L)
+})
+
 test_that("Test Euler problem 67", {
   expect_equal(euler_67(data_67), 7272L)
 })
